e398a7b2dd374b825ec96faf0608629e92fdc2da
sandesh rai <meromainact@gmail.com>
2025-07-03 17:19:28 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # ✅ FIXED: import os
-    uvicorn.run("app:app", host="0.0.0.0", port=port, proxy_headers=True)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run("app:asgi_app", host="0.0.0.0", port=port, proxy_headers=True)
