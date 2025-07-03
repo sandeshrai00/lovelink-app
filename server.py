156f6d9b9df9de6a71a8245e153e52ed123cf4c0
sandesh rai <meromainact@gmail.com>
2025-07-03 17:21:08 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
+from app import socketio, app
 import os
-import uvicorn
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("app:asgi_app", host="0.0.0.0", port=port, proxy_headers=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
