46b6e9bfae646fdca70c2bf43337ca00c0b0d384
sandesh rai <meromainact@gmail.com>
2025-07-04 02:45:07 +0545

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from app import socketio, app
 import os
+from app import app, socketio
+import uvicorn
+from pyvirtualdisplay import Display  # Only if running in a headless environment
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+# If running in a headless environment (like Render), start virtual display
+if os.environ.get('RENDER'):
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    # Use Uvicorn with WebSockets
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+        workers=1,
+        ws='websockets'
+    )
